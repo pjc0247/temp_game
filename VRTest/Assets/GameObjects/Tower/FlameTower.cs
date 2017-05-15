1423f08bf1eb7cba23623a9442f3f731908f3d61
Park June Chul <pjc0247@naver.com>
2017-05-16 01:02:02 +0900

    update

---
+++ VRTest/Assets/GameObjects/Tower/FlameTower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameTower : TowerBase {
+    public Collider flame1, flame2;
+
+    protected override void OnAttack()
+    {
+        var targets = GetTargets();
+
+        foreach (var target in targets)
+        {
+            var targetBounds = target.GetComponent<BoxCollider>().bounds;
+            if (flame1.bounds.Intersects(targetBounds))
+                target.Damage(attackDamage);
+            if (flame2.bounds.Intersects(targetBounds))
+                target.Damage(attackDamage);
+        }
+    }
+}
