7b4db7620f8f41ecd46731a39bc4a4be01f07c65
Park June Chul <pjc0247@naver.com>
2017-05-21 18:37:43 +0900

    update

---
+++ VRTest/Assets/GameObjects/UI/GameHotkey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NewtonVR;
+
+public class GameHotkey : MonoBehaviour {
+
+    void Update() {
+        var rightHand = NVRPlayer.Instance.RightHand;
+
+        var b = rightHand.Inputs[NVRButtons.B].IsPressed;
+        if (b)
+            OnShowMenu();
+	}
+
+    void OnShowMenu()
+    {
+        if (GameObject.Find("Menu") != null)
+            return;
+
+        var menuPrefab = Resources.Load<GameObject>("UI/MenuCanvas");
+        var menu = Instantiate(menuPrefab);
+        menu.name = "Menu";
+        menu.transform.position = new Vector3(0, 4.4f, 0);
+
+        NVRCanvasInput.RegisterCanvas(menu);
+    }
+}
